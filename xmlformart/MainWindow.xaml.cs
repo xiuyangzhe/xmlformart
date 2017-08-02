d0ee39e14d468aaa6c84aebced833fdc96befac6
Rocky Zhou <xiuyangzhe@live.com>
2017-08-02 10:56:56 +0800

    Add project files.

---
+++ xmlformart/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace xmlformart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                output.Text = FormatXml(input.Text);
+            }
+            catch (Exception ex)
+            {
+                output.Text = ex.Message;
+            }
+        }
+        private string FormatXml(string sUnformattedXml)
+        {
+            XmlDocument xd = new XmlDocument();
+            xd.LoadXml(sUnformattedXml);
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+            XmlTextWriter xtw = null;
+            try
+            {
+                xtw = new XmlTextWriter(sw);
+                xtw.Formatting = Formatting.Indented;
+                xtw.Indentation = 1;
+                xtw.IndentChar = '\t';
+                xd.WriteTo(xtw);
+            }
+            finally
+            {
+                if (xtw != null)
+                    xtw.Close();
+            }
+            return sb.ToString();
+        }
+    }
+}
